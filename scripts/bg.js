374180e5cb2db9fc594c8b66ef904d6f41f7db94
Punghyeon <120368342+cuwhenicu@users.noreply.github.com>
2023-11-05 17:31:36 +0900

    API 호출 성공

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,32 +1,37 @@
-
-
-
-
-// 백그라운드 스크립트에서 데이터를 가져오고 팝업 스크립트로 전송
 const getWordListFromAPI = async () => {
-    try {
-      const response = await fetch('https://femiwiki.com/api.php?action=query&format=json&prop=links&titles=%ED%8B%80%3A%EC%A0%A0%EB%8D%94%ED%8F%AD%EB%A0%A5%20%EC%82%AC%EC%9D%B4%EB%93%9C%EB%B0%94&formatversion=2');
-  
-      if (!response.ok) {
-        throw new Error('API 호출 중 오류 발생');
+  try {
+    const response = await fetch(
+      "https://femiwiki.com/api.php?action=query&format=json&prop=links&titles=%ED%8B%80%3A%EC%A0%A0%EB%8D%94%ED%8F%AD%EB%A0%A5%20%EC%82%AC%EC%9D%B4%EB%93%9C%EB%B0%94&formatversion=2"
+    );
+
+    if (!response.ok) {
+      throw new Error("API 호출 중 오류 발생");
+    }
+
+    const data = await response.json();
+    const wordList = [];
+
+    // 'pages' 배열에서 'links' 객체 추출 및 각 링크의 'title'을 'wordList'에 추가
+    data.query.pages.forEach((page) => {
+      if (page.links) {
+        page.links.forEach((link) => {
+          wordList.push(link.title);
+        });
       }
-  
-      const data = await response.json();
-      const wordList = ['데이트 폭력', '페미니즘', '가정폭력']; // 더 이상 하드 코딩하지 않고 API에서 가져올 수 있음
-  
-      return wordList;
-    } catch (error) {
-      console.error('API 호출 중 오류 발생:', error);
-      return [];
-    }
+    });
+
+    return wordList;
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error);
+    return [];
   }
-  
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'showWordList') {
-      getWordListFromAPI().then((wordList) => {
-        sendResponse(wordList.join(', '));
-      });
-      return true;
-    }
-  });
-  
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "showWordList") {
+    getWordListFromAPI().then((wordList) => {
+      sendResponse(wordList.join(", "));
+    });
+    return true;
+  }
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,55 +1,38 @@
-// const getWordListFromAPI = async () => {
-//   fetch(
-//     'https://femiwiki.com/api.php?action=query&format=json&prop=links&titles=%ED%8B%80%3A%EC%A0%A0%EB%8D%94%ED%8F%AD%EB%A0%A5%20%EC%82%AC%EC%9D%B4%EB%93%9C%EB%B0%94&formatversion=2'
-//   )
-//     .then((response) => response.json())
-//     .catch((error) => {
-//       console.error('API 호출 중 오류 발생:', error)
-//     })
-//   const wordList = await new Promise((resolve, reject) =>
-//     resolve(['데이트 폭력', '페미니즘', '가정폭력'])
-//   )
-//   const searchResults = document.querySelectorAll('span > em')
-//   wordList.forEach((word) => {
-//     searchResults.forEach((result) => {
-//       if (result.innerText.includes(word)) {
-//         result.style.backgroundColor = 'rgba(255,238,82,0.25)'
-//       }
-//     })
-//   })
-// }
-
-// // 메시지 수신
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.action === 'showWordList') {
-//     getWordListFromAPI()
-//     sendResponse(request.wordList.join(', '))
-//   }
-// })
-
-
-// content.js
-
-
-// 이벤트 핸들러를 패시브로 설정
-window.addEventListener('touchstart', handleTouchStart, { passive: true });
-
-function handleTouchStart(event) {
-  // 이벤트 처리 로직
-}
-
-
 // 메시지를 백그라운드 스크립트에 보내서 데이터를 가져옴
-chrome.runtime.sendMessage({ action: 'showWordList' }, (response) => {
+chrome.runtime.sendMessage({ action: "showWordList" }, (response) => {
   // 응답 데이터를 처리
-  const wordList = response.split(', ');
-  const searchResults = document.querySelectorAll('span > em');
-  
-  wordList.forEach((word) => {
-    searchResults.forEach((result) => {
-      if (result.innerText.includes(word)) {
-        result.style.backgroundColor = 'rgba(255, 238, 82, 0.25)';
-      }
-    });
-  });
+  const wordList = response.split(", ").map((word) => word.replace(/\s/g, "")); // 단어에서 띄어쓰기 제거
+
+  function walkTheNode(node) {
+    if (node.nodeType === 3) {
+      // 텍스트 노드인 경우
+      const textContentWithoutSpaces = node.textContent.replace(/\s/g, ""); // 노드의 텍스트에서 띄어쓰기 제거
+      wordList.forEach((word) => {
+        if (textContentWithoutSpaces.includes(word)) {
+          // 하이라이트 처리할 텍스트를 찾는 로직
+          const regex = new RegExp(word, "gi");
+          const highlightedText = node.textContent.replace(
+            regex,
+            (match) =>
+              `<span style="background-color: rgba(255, 238, 82, 0.25);">${match}</span>`
+          );
+          const tempDiv = document.createElement("div");
+          tempDiv.innerHTML = highlightedText;
+
+          while (tempDiv.firstChild) {
+            node.parentNode.insertBefore(tempDiv.firstChild, node);
+          }
+          node.parentNode.removeChild(node);
+        }
+      });
+    } else if (
+      node.nodeType === 1 &&
+      node.nodeName !== "SCRIPT" &&
+      node.nodeName !== "STYLE"
+    ) {
+      Array.from(node.childNodes).forEach((child) => walkTheNode(child));
+    }
+  }
+
+  walkTheNode(document.body); // document.body에서 시작하여 모든 노드 순회
 });
