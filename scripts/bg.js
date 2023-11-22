75a811b05fcb968656773de94866be9a57f106c9
Punghyeon <120368342+cuwhenicu@users.noreply.github.com>
2023-11-23 01:09:26 +0900

    api 주소 수정

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,7 +1,7 @@
 const getWordListFromAPI = async () => {
   try {
     const response = await fetch(
-      "https://femiwiki.com/api.php?action=query&format=json&prop=links&titles=%ED%8B%80%3A%EC%A0%A0%EB%8D%94%ED%8F%AD%EB%A0%A5%20%EC%82%AC%EC%9D%B4%EB%93%9C%EB%B0%94&formatversion=2"
+      "https://femiwiki.com/api.php?action=query&format=json&prop=links&titles=%ED%8B%80%3A%EC%A0%A0%EB%8D%94%ED%8F%AD%EB%A0%A5%20%EC%82%AC%EC%9D%B4%EB%93%9C%EB%B0%94&pllimit=69&formatversion=2"
     );
 
     if (!response.ok) {
@@ -30,8 +30,8 @@ const getWordListFromAPI = async () => {
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "showWordList") {
     getWordListFromAPI().then((wordList) => {
-      sendResponse(wordList)
-    })
-    return true
+      sendResponse(wordList);
+    });
+    return true;
   }
 });
